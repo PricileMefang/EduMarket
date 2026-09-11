8aa33ef6e9257d5cbe2b2786d795144a0751205b
Mefang Pricile <mefangpricile41@gmail.com>
2026-09-11 19:39:37 +0100

    Initial commit

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="logos/edumarket logo 2.png">
+    <link rel="stylesheet" href="register.css">
+    <title>EduMarket|Register </title>
+</head>
+<body>
+
+        <header class="site-header">
+            <div class="header-inner">
+                <a href="index.html" class="logo-link">
+                    <img src="logos/edumarket logo 1.png" alt="EduMarket Logo">
+                </a>
+
+                <nav class="main-nav">
+                    <a href="#home">Home</a>
+                    <a href="LogIn.php">LogIn</a>
+                </nav>
+
+                <div class="header-actions">
+                    <a href="Login.php" class="button button-outline">Login</a>
+                    <a href="Register.php" class="button button-primary">Register</a>
+                </div>
+            </div>
+        </header>
+
+</body>
+</html>
